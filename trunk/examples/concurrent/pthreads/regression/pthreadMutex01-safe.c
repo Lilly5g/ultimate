7fd35a835345c8b093ca77aba2d169d4e1b1c631
Frank Schüssele <schuessf@informatik.uni-freiburg.de>
2025-04-30 16:06:48 +0200

    Remove mutex typedefs

--- trunk/examples/concurrent/pthreads/regression/pthreadMutex01-safe.c
+++ trunk/examples/concurrent/pthreads/regression/pthreadMutex01-safe.c
@@ -7,32 +7,6 @@
 #include <stdio.h>
 #include <pthread.h>
 
-typedef struct __pthread_internal_list
-{
-  struct __pthread_internal_list *__prev;
-  struct __pthread_internal_list *__next;
-} __pthread_list_t;
-
-struct __pthread_mutex_s
-{
-  int __lock ;
-  unsigned int __count;
-  int __owner;
-  unsigned int __nusers;
-  int __kind;
- 
-  short __spins; short __elision;
-  __pthread_list_t __list;
- 
-};
-
-typedef union
-{
-  struct __pthread_mutex_s __data;
-  char __size[40];
-  long int __align;
-} pthread_mutex_t;
-
 pthread_mutex_t  mutex;
 
 int main()
